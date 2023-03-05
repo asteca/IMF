02f202880c41e8676387cd51f4fb38a4fd0f04af
Gabriel <gabrielperren@gmail.com>
2023-03-05 11:03:40 -0300

    minor

--- massFunction.py
+++ massFunction.py
@@ -21,18 +21,18 @@ def main():
         fname = str(file).split('/')[1].split('.')[0]
         print("Processing: {}".format(fname))
         print("Reading data from file")
-        mass_mean, mass_std, binar_probs, phot = data_IO.dataRead(
+        mass, mass_std, binar_probs, phot = data_IO.dataRead(
             masses_type, file)
 
         all_Nratios = mass_analysis.singleBinarRatio(
-            binar_cut, mass_mean, binar_probs, phot)
+            binar_cut, mass, binar_probs, phot)
 
         # Mask photometry and masses given the binary probability cut
         if masses_type == 'single':
             bmsk = binar_probs <= binar_cut
         else:
             bmsk = binar_probs >= binar_cut
-        mass_mean_bmsk, mass_std_bmsk, phot_bmsk = mass_mean[bmsk],\
+        mass_bmsk, mass_std_bmsk, phot_bmsk = mass[bmsk],\
             mass_std[bmsk], phot[bmsk]
 
         # For plotting
@@ -40,36 +40,35 @@ def main():
 
         # Apply magnitude range.
         msk = (phot_bmsk[:, 1] >= mag_min) & (phot_bmsk[:, 1] <= mag_max)
-        mass_mean_phot_msk, mass_std_phot_msk = mass_mean_bmsk[msk],\
+        mass_phot_msk, mass_std_phot_msk = mass_bmsk[msk],\
             mass_std_bmsk[msk]
 
         # Apply mass range. Used by the likelihood analysis
-        msk = (mass_mean_phot_msk >= mass_min) &\
-            (mass_mean_phot_msk <= mass_max)
-        mass_mean_mass_msk, mass_std_mass_msk = mass_mean_phot_msk[msk],\
+        msk = (mass_phot_msk >= mass_min) & (mass_phot_msk <= mass_max)
+        mass_mass_msk, mass_std_mass_msk = mass_phot_msk[msk],\
             mass_std_phot_msk[msk]
 
         # Maximum likelihood analysis
         alpha_lkl, alpha_bootstrp, alpha_ranges = mass_analysis.maxLkl(
-            mass_mean_mass_msk, (alpha_min, alpha_max),
-            (Nruns, mass_min, mass_max, mass_std_mass_msk), mass_mean_phot_msk)
+            mass_mass_msk, (alpha_min, alpha_max),
+            (Nruns, mass_min, mass_max, mass_std_mass_msk), mass_phot_msk)
 
         print("Sampling IMFs")
         sampled_IMFs = {}
         for imf in ['Salpeter (55)', 'Miller-Scalo (79)', 'Kroupa (01)',
                     'Chabrier (03, indiv)', 'Chabrier (03, system)']:
-            print("  {}".format(imf))
-            mass_IMF = IMF.invTrnsfSmpl(mass_mean_mass_msk, imf)
+            # print("  {}".format(imf))
+            mass_IMF = IMF.invTrnsfSmpl(mass_mass_msk, imf)
             Lkl_IMF, bootstrp_IMF = mass_analysis.maxLkl(
                 mass_IMF, (alpha_min, alpha_max),
                 (Nruns, mass_min, mass_max, None), [])
             sampled_IMFs[imf] = (mass_IMF, Lkl_IMF, bootstrp_IMF)
 
         makePlot.main(
-            masses_type, binar_cut, mag_min, mag_max, mass_min, mass_max,
-            Nruns, all_Nratios,
-            binar_probs, phot_bin_used, phot_bin_unused, mass_mean_phot_msk,
-            mass_mean_mass_msk, alpha_lkl, alpha_bootstrp, alpha_ranges,
+            fname, masses_type, binar_cut, mag_min, mag_max, mass_min,
+            mass_max, Nruns, all_Nratios,
+            binar_probs, phot_bin_used, phot_bin_unused, mass_phot_msk,
+            mass_mass_msk, alpha_lkl, alpha_bootstrp, alpha_ranges,
             alpha_min, alpha_max, sampled_IMFs)
 
 
